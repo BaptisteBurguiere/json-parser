9fc70f1595e3cb7aeea9a6bc3a26235e01de4cf3
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2024-02-19 17:26:17 +0100

    temp commit

--- src/lib.rs
+++ src/lib.rs
@@ -39,28 +39,147 @@ pub mod JsonParser
                 JsonValue::Dict(map) =>
                 {
                     map.insert(key, value);
-                    Ok(());
+                    Ok(())
                 },
-                _ =>
+                _ => Err("Cannot insert in a map if JsonValue is not Dict type")
+            }
+        }
+
+        fn insert_vec(&self, value: JsonValue) -> Result<(), &'static str>
+        {
+            match &self
+            {
+                JsonValue::List(vec) =>
                 {
-                    Err("Cannot insert in a map if JsonValue is not Dict type");
-                }
+                    vec.push(value);
+                    Ok(())
+                },
+                _ => Err("Cannot insert in a vector if JsonValue is not List type")
             }
         }
     }
 
     fn getFileContent(file_path: String) -> Result<String, Box<dyn Error>>
     {
-        let file_content = fs::read_to_string(file_path)?;
-        Ok(file_content)
+        let mut file_content = fs::read_to_string(file_path)?;
+        file_content.trim();
+
+        let mut return_str = String::new();
+        while file_content.len() > 0
+        {
+            if file_content.starts_with("\"")
+            {
+                return_str.push(file_content.remove(0));
+                while !file_content.starts_with("\"")
+                {
+                    return_str.push(file_content.remove(0));
+                }
+                return_str.push(file_content.remove(0));
+            }
+            else if file_content.starts_with("\'")
+            {
+                return_str.push(file_content.remove(0));
+                while !file_content.starts_with("\'")
+                {
+                    return_str.push(file_content.remove(0));
+                }
+                return_str.push(file_content.remove(0));
+            }
+            else if file_content.starts_with(" ") || file_content.starts_with("\t") || file_content.starts_with("\n")
+            {
+                file_content.remove(0);
+            }
+            else
+            {
+                return_str.push(file_content.remove(0))
+            }
+        }
+        
+        Ok(return_str)
     }
 
-    fn parseMap(file_path: String) -> Result<JsonValue, &'static str>
+    fn parseKey(mut file_content: String) -> Result<(String, String), &'static str>
+    {
+        match file_content.remove(0)
+        {
+            '\"' =>
+            {
+                let mut key = String::new();
+                while file_content.len() > 0 && file_content.starts_with("\"")
+                {
+                    key.push(file_content.remove(0));
+                }
+                if file_content.len() > 0
+                {
+                    file_content.remove(0);
+                }
+
+                if file_content.len() == 0 || !file_content.starts_with(":")
+                {
+                    return Err("Wrong key format");
+                }
+                file_content.remove(0);
+                Ok((file_content, key))
+            }
+            '\'' =>
+            {
+                let mut key = String::new();
+                while file_content.len() > 0 && file_content.starts_with("\'")
+                {
+                    key.push(file_content.remove(0));
+                }
+                if file_content.len() > 0
+                {
+                    file_content.remove(0);
+                }
+    
+                if file_content.len() == 0 || !file_content.starts_with(":")
+                {
+                    return Err("Wrong key format");
+                }
+                file_content.remove(0);
+                Ok((file_content, key))
+            }
+            _ =>
+            {
+                Err("Wrong key format")
+            }
+        }
+    }
+
+    fn parseMap(mut file_content: String) -> Result<(String, JsonValue), &'static str>
     {
         let return_map = JsonValue::Dict(HashMap::new());
+        file_content.remove(0);
+
+        loop
+        {
+            let mut key = String::new();
+            (file_content, key) = parseKey(file_content)?;
+
+            match file_content.as_bytes()[0]
+            {
+                b'{' =>
+                {
+                    let value = JsonValue::Dict(HashMap::new());
+                    return_map.insert_map(key, value);
+                }
+                b'[' =>
+                {
+                    let value = JsonValue::List(Vec::new());
+                    return_map.insert_map(key, value);
+                }
+                _ =>
+                {
+
+                }
+            }
+        }
+
+        Ok((file_content, return_map))
     }
 
-    fn parseVec(file_path: String) -> Result<JsonValue, &'static str>
+    fn parseVec(file_content: String) -> Result<(String, JsonValue), &'static str>
     {
         
     }
@@ -68,17 +187,16 @@ pub mod JsonParser
     pub fn parse(file_path: String) -> Result<JsonValue, Box<dyn Error>>
     {
         let mut file_content = getFileContent(file_path)?;
-        file_content.trim();
 
         let mut json_obj: JsonValue;
 
         if file_content.starts_with("{")
         {
-            json_obj = parseMap(file_content)?;
+            (file_content, json_obj) = parseMap(file_content)?;
         }
         else if file_content.starts_with("[")
         {
-            json_obj = parseVec(file_content)?;
+            (file_content, json_obj) = parseVec(file_content)?;
         }
 
         Ok(json_obj)
